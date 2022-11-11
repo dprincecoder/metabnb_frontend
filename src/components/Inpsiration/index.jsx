8840bd0dd2288808976de20b0ab5767f397e4a68
dprincecoder <dprincecoder@gmail.com>
2022-11-11 10:04:00 +0100

    created the inspiration gallery

--- src/components/Inpsiration/index.jsx
+++ src/components/Inpsiration/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./inspiration.css";
 
 //datas
 const data = [
@@ -78,20 +79,32 @@ const data = [
 
 const Inspiration = () => {
   return (
-    <section>
-      <h1 className="red-rose-font">Inspiration for your next adventure</h1>
-      <div className="data-img-grid">
+    <section className="container inspiration">
+      <h1 className="red-rose-font text-center">
+        Inspiration for your next adventure
+      </h1>
+      <div className="data-img img-grid-items-4">
         {data.map((item) => (
           <div className="data-img-grid-item" key={item.id}>
-            <img src={item.img} alt={item.name} />
-            <div className="data-img-grid-item-desc">
-              <h3>{item.name}</h3>
-              <p>{item.desc}</p>
-              <div className="">
+            <img
+              src={item.img}
+              alt={item.name}
+              className="inspiration-data-img"
+            />
+            <div className="data-img-grid-item-desc width-full space-up-down text-dark">
+              <div className="item-flex items-center justify-between">
+                <h3 className="inspiration-data-title red-rose-font">
+                  {item.name}
+                </h3>
+                <p className="inspiration-data-desc">{item.desc}</p>
+              </div>
+              <div className="inspiration-duration-item item-flex items-center justify-between">
                 <p>{item.distance}</p>
                 <p>{item.duration}</p>
               </div>
-              <img src={item.rating} alt="rating" />
+              <div className="inspiration-data-img-rating">
+                <img src={item.rating} alt="rating" className="" />
+              </div>
             </div>
           </div>
         ))}
